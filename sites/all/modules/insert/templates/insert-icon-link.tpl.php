71d9ec3bd04a00f1e70d7bc11eefe5a7c947ba9c
Jackson Dockery <jhdocker@gmail.com>
2015-08-06 07:54:34 -0400

    still trying to fix image issue

--- sites/all/modules/insert/templates/insert-icon-link.tpl.php
+++ sites/all/modules/insert/templates/insert-icon-link.tpl.php
@@ -22,3 +22,4 @@
  * what about the div?
  */
 ?>
+<div class="file"><?php print $icon; ?> <a href="<?php print $url ?>"<?php print $class ? ' class="' . $class . '"' : '' ?> title="__description__">__description_or_filename__</a></div>
